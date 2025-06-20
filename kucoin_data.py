73f1a205689146e00c45843ad0f3f65575e492f3
NiuStar-Byte <nius.tigan@gmail.com>
2025-06-20 20:11:31 +0700

    Update kucoin_data.py

--- kucoin_data.py
+++ kucoin_data.py
@@ -4,15 +4,16 @@ import pandas as pd
 def fetch_ohlcv(symbol, tf):
     try:
         tf_map = {
+            "2min": "2min",
             "3min": "3min",
             "5min": "5min"
         }
-
         if tf not in tf_map:
             print(f"[{symbol}] Unsupported timeframe: {tf}")
             return None
 
         url = f"https://api.kucoin.com/api/v1/market/candles?type={tf_map[tf]}&symbol={symbol}"
+
         res = requests.get(url)
         data = res.json()
 
@@ -20,9 +21,11 @@ def fetch_ohlcv(symbol, tf):
             print(f"[{symbol}] No OHLCV data fetched.")
             return None
 
+        # Format into dataframe
         df = pd.DataFrame(data["data"], columns=["timestamp", "open", "close", "high", "low", "volume", "turnover"])
-        df = df.iloc[::-1]  # reverse to chronological
+        df = df.iloc[::-1]  # reverse to chronological order
         df[["open", "high", "low", "close", "volume"]] = df[["open", "high", "low", "close", "volume"]].astype(float)
+
         return df
 
     except Exception as e:
