4a0770b3ecffd61adbdd930cdd3e27f6b73819b2
NiuStar-Byte <nius.tigan@gmail.com>
2025-07-02 11:01:33 +0700

    Create pecnew_config.py

---
+++ pecnew_config.py
@@ -0,0 +1,8 @@
+# pecnew_config.py
+
+# Flag to switch between PEC and PECNew
+use_PECNew = True  # Set to True to activate PECNew and backtest mode, False for PEC
+
+# Global variables for backtest mode and historical data (e.g., OHLCV data)
+backtest_mode = True  # Set this to False for live trading
+historical_data = None  # This should be replaced with actual historical data (list of bars)
