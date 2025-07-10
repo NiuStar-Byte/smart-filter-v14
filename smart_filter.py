19ad9c15e32e9b42db0335915959e05c97a90d28
NiuStar-Byte <nius.tigan@gmail.com>
2025-07-10 11:04:24 +0700

    Update smart_filter.py

--- smart_filter.py
+++ smart_filter.py
@@ -16,7 +16,7 @@ class SmartFilter:
         df3m: pd.DataFrame = None,
         df5m: pd.DataFrame = None,
         tf: str = None,
-        min_score: int = 14,
+        min_score: int = 15,
         required_passed: int = 12,      # NEW: now 10 (for 17 gatekeepers)
         volume_multiplier: float = 2.0,
         kwargs = None
