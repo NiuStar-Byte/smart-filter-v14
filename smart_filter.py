b70ad6c64963ffeba2fd72836a7e025cf030ae27
NiuStar-Byte <nius.tigan@gmail.com>
2025-07-18 12:51:32 +0700

    Update smart_filter.py

--- smart_filter.py
+++ smart_filter.py
@@ -41,7 +41,7 @@ class SmartFilter:
         min_score: int = 15,
         required_passed: int = 11,      # NEW: now 11 (for 17 gatekeepers)
         volume_multiplier: float = 2.0,
-        liquidity_threshold: float = 0.5,   # <-- Set a default value
+        liquidity_threshold: float = 0.2,   # <-- Set a default value
         kwargs = None
     ):
         if kwargs is None:
