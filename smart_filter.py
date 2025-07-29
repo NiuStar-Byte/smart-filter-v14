3bdc96882397a1281c96ee034accdc31b50f2eca
NiuStar-Byte <nius.tigan@gmail.com>
2025-07-29 09:30:46 +0700

    Update smart_filter.py

--- smart_filter.py
+++ smart_filter.py
@@ -48,7 +48,7 @@ class SmartFilter:
         df3m: pd.DataFrame = None,
         df5m: pd.DataFrame = None,
         tf: str = None,
-        min_score: int = 10,
+        min_score: int = 11,
         required_passed: int = 7,      # NEW logic 100% : now 7 (for 7 gatekeepers)
         volume_multiplier: float = 2.0,
         liquidity_threshold: float = 0.25,   # <-- Set a default value
