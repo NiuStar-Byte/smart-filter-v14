eea845457cb3c0f47b95210981c91cf26650ee56
NiuStar-Byte <nius.tigan@gmail.com>
2025-07-31 14:42:11 +0700

    Update smart_filter.py

--- smart_filter.py
+++ smart_filter.py
@@ -144,7 +144,7 @@ class SmartFilter:
             "Support/Resistance"
         ]
 
-        self.soft_gatekeepers = []
+        self.soft_gatekeepers = ["Volume Spike"]
         
         # Directional-aware filters are those with different weights between LONG and SHORT
 #        self.directional_aware_filters = [
