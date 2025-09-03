495ec8ef1e5e3337b00e720d829784aae8a9854b
NiuStar-Byte <nius.tigan@gmail.com>
2025-09-03 13:24:55 +0700

    Change default min_score from 8 to 6

--- smart_filter.py
+++ smart_filter.py
@@ -26,7 +26,7 @@ class SmartFilter:
         df3m: Optional[pd.DataFrame] = None,
         df5m: Optional[pd.DataFrame] = None,
         tf: Optional[str] = None,
-        min_score: int = 8,
+        min_score: int = 6,
         required_passed: Optional[int] = None,  # int or None allowed
         volume_multiplier: float = 2.0,
         liquidity_threshold: float = 0.50,
