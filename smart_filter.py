ffff1bb49ddd97028c0b73cddd35e1b5f26dbca0
NiuStar-Byte <nius.tigan@gmail.com>
2025-10-27 20:47:14 +0700

    Update smart_filter.py

--- smart_filter.py
+++ smart_filter.py
@@ -27,7 +27,7 @@ class SmartFilter:
         df3m: Optional[pd.DataFrame] = None,
         df5m: Optional[pd.DataFrame] = None,
         tf: Optional[str] = None,
-        min_score: int = 12,
+        min_score: int = 11,
         required_passed: Optional[int] = None,  # int or None allowed
         volume_multiplier: float = 2.0,
         liquidity_threshold: float = 0.25,
